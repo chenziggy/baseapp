cb4dbd204f33ca2225186204d1397117c43e3c94
Ziggy Chen <ziggycs9796@hotmail.com>
2024-04-15 17:02:43 +0800

    feat: 增加过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <header @click="renderRoutes" class="h-20 bg-blue">
     <!-- routes: {{ routes }} -->
-    
+
   </header>
-  <div class="flex h-full">
-    <aside class="w-40 bg-amber h-full">menu</aside>
-    <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-      <router-view v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" />
-    </keep-alive>
-  </router-view>  </main>
+  <div class=" overflow-hidden flex h-full">
+    <aside class="w-40 bg-amber h-full shrink-0">menu</aside>
+    <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200 " class="w-full h-full">
+      <router-view v-slot="{ Component}">
+        <Transition name="fade" mode="out-in">
+            <KeepAlive>
+
+                <component :is="Component"  />
+              </KeepAlive>
+
+        </Transition>
+      </router-view>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useRouter,  } from 'vue-router'
-import {onMounted, ref} from 'vue'
+import { useRouter, } from 'vue-router'
+import { ref } from 'vue'
 const router = useRouter()
 
-const routes =  ref()
+const routes = ref()
 
 function renderRoutes() {
   routes.value = JSON.stringify(router.getRoutes())
   console.log(routes.value)
 }
 </script>
+
+<style>
+.fade-enter-active
+,.fade-leave-active
+{
+  transition: opacity .2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}</style>
